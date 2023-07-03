013ed334c5fdee97608e19d29a3e2facb1ddf790
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2023-07-03 16:39:47 -0500

    Se crea la función para el envío de formulario a un correo electrónico

---
+++ php/contact.php
@@ -0,0 +1,39 @@
+<?php
+
+// Success Message
+$success = "";
+$error = "Failed";
+
+require("PHPMailer.php");
+require("SMTP.php");
+
+$nombre     	= strip_tags($_POST['nombre']);
+$correo      = strip_tags($_POST['correo']);
+$celular     	= strip_tags($_POST['celular']);
+$selectSeguro     	= strip_tags($_POST['selectSeguro']);
+$mensaje     	= strip_tags($_POST['mensaje']);
+$asunto     	= '';
+}
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "eyeseguros.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "euyenidyaver@eyeseguros.com";
+$mail->Password = "Eugerente077@";
+$mail->SetFrom("euyenidyaver@eyeseguros.com");
+$mail->Subject = $asunto;
+$mail->Body = "<html><body>$body</body></html>";
+$mail->AddAddress("euyenidyaver@eyeseguros.com");
+$mail->AddAddress("edderrojas@eyeseguros.com");
+
+
+if($mail->Send()) {
+	echo "$success";
+} else {
+	echo "$error";
+}
