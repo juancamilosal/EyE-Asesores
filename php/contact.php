fba43fe14a64764a99daa45035c9895bcab2f5d9
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2023-07-07 10:10:56 -0500

    Se hace unas ajustes

--- php/contact.php
+++ php/contact.php
@@ -20,12 +20,12 @@ $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
 $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "eyeseguros.com";
+$mail->Host = "euyenidyaver@eyeseguros.com";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
 $mail->Username = "euyenidyaver@eyeseguros.com";
-$mail->Password = "Eugerente077@";
-$mail->SetFrom("euyenidyaver@eyeseguros.com");
+$mail->Password = "{R,{?5@HdkIo";
+$mail->SetFrom("contacto@eyeseguros.com");
 $mail->Subject = $asunto;
 $mail->Body = "<html><body>$body</body></html>";
 $mail->AddAddress("euyenidyaver@eyeseguros.com");
